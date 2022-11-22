582d5ceefed910c5aacb2ce8200f184abea215d4
Diggy <valentinewilson@gmail.com>
2022-11-22 20:07:59 +0000

    - Added index.html - Added request router

---
+++ src/main/java/ase/newportreuseandrecycle/web/RequestRouter.java
@@ -0,0 +1,13 @@
+package ase.newportreuseandrecycle.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class RequestRouter implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("index");
+    }
+}
