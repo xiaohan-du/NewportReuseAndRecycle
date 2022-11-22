7ab8a899cefe437ef0cd9c6a0f307bc645bf9b57
Xiaohan Du <dux18@cardiff.ac.uk>
2022-11-22 13:14:18 +0000

    Initial push

---
+++ src/main/java/ase/newportreuseandrecycle/NewportReuseAndRecycleApplication.java
@@ -0,0 +1,13 @@
+package ase.newportreuseandrecycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewportReuseAndRecycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewportReuseAndRecycleApplication.class, args);
+    }
+
+}
