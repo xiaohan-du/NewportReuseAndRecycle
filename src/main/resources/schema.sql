077c6fb50a6df3671cca78d58a2cf245fbe12995
Xiaohan Du <dux18@cardiff.ac.uk>
2022-11-29 12:01:28 +0000

    Change the data type

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,10 +11,10 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users
 (
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    `username` varchar(45) NOT NULL,
-    `password` varchar(64) NOT NULL,
-    `role` varchar(45) NOT NULL,
-    `enabled` tinyint(4) DEFAULT NULL,
+    `username` VARCHAR(45) NOT NULL,
+    `password` VARCHAR(64) NOT NULL,
+    `role` VARCHAR(45) NOT NULL,
+    `enabled` BIT DEFAULT NULL,
     PRIMARY KEY (`id`)
     )
     engine = InnoDB;
