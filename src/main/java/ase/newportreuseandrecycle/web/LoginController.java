2ec35630344a695425ab207243bae2078cbbad21
Xiaohan Du <dux18@cardiff.ac.uk>
2022-11-29 12:03:02 +0000

    Minor change

--- src/main/java/ase/newportreuseandrecycle/web/LoginController.java
+++ src/main/java/ase/newportreuseandrecycle/web/LoginController.java
@@ -15,7 +15,6 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.Optional;
 
 @Controller
-//@RequestMapping("/")
 public class LoginController {
     private final UserService userService;
     public LoginController(UserService svc) {
