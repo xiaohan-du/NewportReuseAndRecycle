6422de8e599095ac2aea9912fe770003b6b16242
Diggy <valentinewilson@gmail.com>
2022-12-01 16:41:03 +0000

    Using API for fetching product listings information rather than MVC model + thymeleaf

--- src/main/resources/static/js/listing.js
+++ src/main/resources/static/js/listing.js
@@ -1,5 +1,5 @@
 async function fetchListing() {
-    const response = await fetch('http://localhost:8080/api/listing');
+    const response = await fetch("http://localhost:8080/api/listing");
     if (!response.ok) {
         const message = `An error has occured: ${response.status}`;
         throw new Error(message);
@@ -8,8 +8,53 @@ async function fetchListing() {
         return listings;
     }
 }
-document.getElementById("itemList").innerHTML = "Paragraph changed.";
 
-fetchListing().then(listings => {
-    console.log(listings);
+fetchListing().then((listings) => {
+    const NUM_OF_COLS = 3;
+    let numOfRows = listings.length / NUM_OF_COLS + 1;
+
+    let listingsGridElement = document.getElementById("listings-grid");
+
+    for (let i = 0; i < numOfRows; i++) {
+        let row = document.createElement("row");
+        row.className = "row";
+
+        for (let j = 0; j < NUM_OF_COLS; j++) {
+            let curIndex = (NUM_OF_COLS * i) + j;
+
+            if (curIndex == listings.length) {
+                listingsGridElement.appendChild(row);
+                return;
+            }
+
+            let col = document.createElement("div");
+            col.className = "col";
+
+            let img = document.createElement("img");
+            img.src = listings[curIndex].imageUrl;
+            img.width = img.height = "128";
+
+            let pTitle = document.createElement("p");
+            pTitle.innerText = listings[curIndex].title;
+
+            let pDescription = document.createElement("p");
+            pDescription.innerText = listings[curIndex].description;
+
+            let pUserID = document.createElement("p");
+            pUserID.innerText = listings[curIndex].userId;
+
+            let pPrice = document.createElement("p");
+            pPrice.innerText = listings[curIndex].price;
+
+            col.appendChild(img);
+            col.appendChild(pTitle);
+            col.appendChild(pDescription);
+            col.appendChild(pUserID);
+            col.appendChild(pPrice);
+
+            row.append(col);
+        }
+
+        listingsGridElement.append(row);
+    }
 });
