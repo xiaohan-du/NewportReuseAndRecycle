8516753b840743ee3d0b2860fbe7257a27947fbe
Xiaohan Du <dux18@cardiff.ac.uk>
2022-12-02 23:38:42 +0000

    Remove commented lines

--- src/main/java/ase/newportreuseandrecycle/service/ListingServiceImpl.java
+++ src/main/java/ase/newportreuseandrecycle/service/ListingServiceImpl.java
@@ -15,15 +15,6 @@ public class ListingServiceImpl implements ListingService{
 
     @Override
     public List<ListingDto> getListings() {
-
-//        ListingDto testProduct1 = new ListingDto("Product 1", "Lorem ipsum", 19.99, "http://pamsdailydish.com/wp-content/uploads/2015/04/Bunch-Bananas-2.jpg");
-//        ListingDto testProduct2 = new ListingDto("Product 2", "some description", 5.99, "http://pamsdailydish.com/wp-content/uploads/2015/04/Bunch-Bananas-2.jpg");
-//        ListingDto testProduct3 = new ListingDto("Product 3", "abcdefghijklmnop", 2.50, "http://pamsdailydish.com/wp-content/uploads/2015/04/Bunch-Bananas-2.jpg");
-//        ListingDto testProduct4 = new ListingDto("Product 4", "123456789", 10.00, "http://pamsdailydish.com/wp-content/uploads/2015/04/Bunch-Bananas-2.jpg");
-//
-//        List<ListingDto> listingDtos = List.of(testProduct1, testProduct2, testProduct3, testProduct4);
-
-
         List<Listing> listings = listingRepository.getListings();
         return ListingAssembler.toDto(listings);
     }
