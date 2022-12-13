b763b3e454e2a6e3c8209c4e3d4ab40336e1a14a
Xiaohan Du <dux18@cardiff.ac.uk>
2022-12-13 15:27:54 +0000

    Tidy up unnecessary code

--- src/test/java/ase/newportreuseandrecycle/GetListingsMockRepo.java
+++ src/test/java/ase/newportreuseandrecycle/GetListingsMockRepo.java
@@ -7,7 +7,9 @@ import ase.newportreuseandrecycle.service.message.ListingRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -17,12 +19,16 @@ public class GetListingsMockRepo {
     @Autowired
     private ListingRepository listingRepository;
 
+    private String testCategory = "food";
+    private Integer testExistingId = 4;
+    private Integer testNewId = 5;
+
     @Test
     public void shouldGetAListingByValidCategory() {
         // GIVEN
         ListingRequest listingRequest = ListingRequest.of().build();
         // WHEN
-        var listingResponse = listingService.getListingsByCategory(listingRequest, "food");
+        var listingResponse = listingService.getListingsByCategory(listingRequest, testCategory);
         // THEN
         assertEquals(2, listingResponse.getListings().size());
     }
@@ -50,15 +56,39 @@ public class GetListingsMockRepo {
     }
 
     @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
     public void shouldAddAListingToRepo() {
         // GIVEN
-        Listing l5 = new Listing(5, 1, "test title 1", "test description 1", 1.99, "http://www.clker.com/cliparts/f/Z/G/4/h/Q/no-image-available-hi.png", "test category 1", "collection", 11.11, 22.22);
+        Listing l5 = new Listing(testNewId, 1, "test title 1", "test description 1", 1.99, "http://www.clker.com/cliparts/f/Z/G/4/h/Q/no-image-available-hi.png", "test category 1", "collection", 11.11, 22.22);
         listingRepository.addNewListing(l5);
         ListingRequest listingRequest = ListingRequest.of().build();
         // WHEN
-        var listingResponse = listingService.getAListingById(listingRequest, 5);
+        var listingResponse = listingService.getAListingById(listingRequest, testNewId);
         // THEN
-        assertEquals(5, listingResponse.getListingDto().getId());
-        listingRepository.deleteListingById(5);
+        assertEquals(testNewId, listingResponse.getListingDto().getId());
+    }
+
+    @Test
+    public void shouldGetListingsByCategory() {
+        // GIVEN
+        listingRepository.getListingsByCategory(testCategory);
+        ListingRequest listingRequest = ListingRequest.of().build();
+        // WHEN
+        var listingResponse = listingService.getListingsByCategory(listingRequest, testCategory);
+        // THEN
+        assertEquals(2, listingResponse.getListings().size());
+    }
+
+    @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    public void shouldDeleteListingById() {
+        // GIVEN
+        Optional<Listing> listingFour = listingRepository.getAListingById(testExistingId);
+        listingRepository.deleteListingById(testExistingId);
+        ListingRequest listingRequest = ListingRequest.of().build();
+        // WHEN
+        var listingResponse = listingService.getListings(listingRequest);
+        // THEN
+        assertEquals(3, listingResponse.getListings().size());
     }
 }
