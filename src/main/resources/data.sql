da9dee73192d475e295f9938cd16bb0be7e9917b
Xiaohan Du <dux18@cardiff.ac.uk>
2023-01-01 23:17:51 +0000

    Minor changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,8 +10,8 @@ VALUES ('user',
 INSERT INTO listing (title, description, price, image_url, user_id, category, latitude, longitude)
 VALUES ('test listing 1', 'This is text listing 1 content', 3.99, 'http://www.clker.com/cliparts/f/Z/G/4/h/Q/no-image-available-hi.png', 1, 'food', 51.4902713, -3.1766767),
        ('test listing 2', 'This is text listing 2 content', 13.99, 'http://www.clker.com/cliparts/f/Z/G/4/h/Q/no-image-available-hi.png', 1, 'food', 51.4902713, -3.1766767),
-       ('test listing 3', 'This is text listing 3 content', 200.00, 'http://www.clker.com/cliparts/f/Z/G/4/h/Q/no-image-available-hi.png', 1, 'electronics', 51.4902713, -3.1766767),
-       ('test listing 4', 'This is text listing 4 content', 4.50, 'http://www.clker.com/cliparts/f/Z/G/4/h/Q/no-image-available-hi.png', 1, 'furniture', 51.4902713, -3.1766767);
+       ('test listing 3', 'This is text listing 3 content', 200.00, 'http://www.clker.com/cliparts/f/Z/G/4/h/Q/no-image-available-hi.png', 2, 'electronics', 51.4902713, -3.1766767),
+       ('test listing 4', 'This is text listing 4 content', 4.50, 'http://www.clker.com/cliparts/f/Z/G/4/h/Q/no-image-available-hi.png', 2, 'furniture', 51.4902713, -3.1766767);
 
 INSERT INTO category (category)
 VALUES ('food'),
